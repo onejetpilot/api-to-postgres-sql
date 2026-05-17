5d7b416252564b151833079bce1255d99a2596e8
Mikhail Razhin <thxnp@ya.ru>
2026-05-17 13:36:31 +0300

    feat: add exchange rate transformation

---
+++ src/transform.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+def transform_exchange_rates(row_data: list[dict]) -> pd.DataFrame:
+    """
+    Преобразует данные о курсах валют в DataFrame
+    """
+    df = pd.DataFrame(row_data)
+
+    df = df.rename(
+        columns={
+            'date': 'rate_date',
+            'base': 'base_currency',
+            'quote': 'quote_currence',
+        }
+    )
+
+    df['rate_date'] = pd.to_datetime(df['rate_date']).dt.date
+    df['base_currency'] = df['base_currency'].astype(str)
+    df['quote_currency'] = df['quote_currence'].astype(str)
+    df['rate'] = df['rate'].astype(float)
+
+    df = df.drop_duplicates(subset=['rate_date', 'base_currency', 'quote_currency'])
+
+    df = df.sort_values(by=['rate_date', 'base_currency', 'quote_currency']).reset_index(drop=True)
+
+    return df
+
+if __name__ == "__main__":
+    test_data = [
+        {"date": "2024-01-01", "base": "EUR", "quote": "CHF", "rate": 0.92743},
+        {"date": "2024-01-01", "base": "EUR", "quote": "CHF", "rate": 0.92743},
+        {"date": "2024-01-01", "base": "EUR", "quote": "USD", "rate": 1.1064},
+    ]
+
+    result = transform_exchange_rates(test_data)
+
+    print(result)
+    print(result.dtypes)
+      
